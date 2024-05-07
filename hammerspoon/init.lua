8a00a15e20899df213ab6ee929130b0abe96fc30
Howa <zhonghao@dayuwuxian.com>
2024-05-07 11:38:49 +0800

    feat: find file with enter path

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,27 +1,49 @@
 -- with locatorjs
 -- hammerspoon://openFile?path=${filePath}&line=${line}&column=${column}
 
+-- 设置 Neovim 可执行文件的路径
+local nvimPath = "/opt/homebrew/bin/nvim"
+
 hs.urlevent.bind("openFile", function(eventName, params)
-  hs.printf("Received event: " .. eventName)
-  hs.printf("Params: " .. hs.inspect(params))
+  hs.printf("openFile event received")
 
   local filePath = params["path"]
   local line = tonumber(params["line"])
   local column = tonumber(params["column"])
 
-  hs.printf("File path: " .. (filePath or "nil"))
-  hs.printf("Line: " .. (line or "nil"))
-  hs.printf("Column: " .. (column or "nil"))
+  hs.printf("File path: %s", filePath)
+  hs.printf("Line: %d", line)
+  hs.printf("Column: %d", column)
 
   if filePath ~= nil and line ~= nil and column ~= nil then
-    local nvimBin = "/opt/homebrew/bin/nvim"
-    local command = string.format("%s %s +'execute \"normal! %dG%d|\"'", nvimBin, filePath, line, column)
-    hs.printf("Executing command: " .. command)
+    hs.printf("Neovim path: %s", nvimPath)
+
+    -- 使用 Lua 的内置函数获取目录路径
+    local dirPath = string.match(filePath, "(.*[/\\])")
+
+    if not dirPath then
+      hs.printf("Failed to get directory path")
+      return
+    end
+
+    hs.printf("Directory path: %s", dirPath)
+
+    local command = string.format("cd %q && %s %q +'execute \"normal! %dG%d|\"'",
+      dirPath, nvimPath, filePath, line, column)
+
+    hs.printf("Executing command: %s", command)
 
     local scriptFile = os.tmpname()
+    hs.printf("Temporary script file: %s", scriptFile)
+
     local file = io.open(scriptFile, "w")
-    file:write(command)
-    file:close()
+    if file then
+      file:write(command)
+      file:close()
+      hs.printf("Command written to script file")
+    else
+      hs.printf("Failed to create script file")
+    end
 
     local appleScript = [[
       tell application "iTerm2"
@@ -30,6 +52,9 @@ hs.urlevent.bind("openFile", function(eventName, params)
           tell current session of current window to write text "bash ]] .. scriptFile .. [["
       end tell
     ]]
+
+    hs.printf("Executing AppleScript: %s", appleScript)
+
     local success, result, output = hs.osascript.applescript(appleScript)
     if success then
       hs.printf("Command executed successfully.")
@@ -43,4 +68,3 @@ hs.urlevent.bind("openFile", function(eventName, params)
     hs.printf("Invalid parameters, not executing command")
   end
 end)
-
